ab76b6aa86f2242e4fb7c94ae3c7a1363edd91e7
Arex360 <49303647+Arex360@users.noreply.github.com>
2020-02-07 17:17:26 +0500

    Create RomanToInteger.js

---
+++ RomanToInteger.js
@@ -0,0 +1,38 @@
+ans = 0
+r = 'CD'
+for(i=0;i<r.length;i++){
+  if(r[i] == 'I'){
+    ans = ans + 1
+  }
+  else if(r[i] == 'V'){
+    ans = ans + 5
+    if(r[i-1] == 'I'){
+      ans = ans - 2 
+    }
+  }
+  else if(r[i] == 'X'){
+    ans = ans + 10
+    if(r[i-1] == 'I' ){
+      ans = ans - 2
+    }
+  }
+  else if(r[i] == 'L'){
+    ans = ans + 50
+    if(r[i-1] == 'X'){
+      ans = ans - 11
+    }
+  }
+  else if(r[i] == 'C'){
+    ans = ans + 100
+    if(r[i-1] == 'X'){
+      ans = ans - 11
+    }
+  }
+  else if(r[i] == 'D'){
+    ans = ans + 500
+    if(r[i-1] == 'C'){
+      ans = ans - 200
+    }
+  }
+}
+console.log(ans)
